5ceabe11a731742ba76e497e369b2a958815d728
TomasMartin2004 <tomassmartin04@gmail.com>
2024-09-13 23:50:44 -0300

    feat: model, serializer y view de producto

--- backend/ecommerce/productos/models.py
+++ backend/ecommerce/productos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import django_filters
 
 class Productos(models.Model):
     nombre = models.CharField(max_length=100)
@@ -9,3 +10,11 @@ class Productos(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class ProductosFilter(django_filters.FilterSet):
+    nombre = django_filters.CharFilter(field_name='nombre', lookup_expr='icontains')
+    precioMinimo = django_filters.NumberFilter(field_name='precio', lookup_expr='gte')
+    precioMaximo = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
+    class Meta:
+        model = Productos
+        fields = ['nombre', 'precioMinimo', 'precioMaximo']
--- backend/ecommerce/productos/views.py
+++ backend/ecommerce/productos/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import ProductosSerializer
-from .models import Productos
+from .models import Productos, ProductosFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
+class ProductosPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class ProductosList(generics.ListCreateAPIView):
     queryset = Productos.objects.all()
     serializer_class = ProductosSerializer
+    pagination_class = ProductosPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ProductosFilter
 
 class ProductosDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Productos.objects.all()
