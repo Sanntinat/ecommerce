99e140dd622c5f355d26d27652d3eb0561aec139
Sanntinat <messinatalichio@gmail.com>
2024-09-23 20:18:40 -0300

    elimino filtros y paginacion de categorias

--- backend/ecommerce/categoria/models.py
+++ backend/ecommerce/categoria/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import django_filters
+
 
 class Categorias(models.Model):
     nombre = models.CharField(max_length=100)
@@ -8,9 +8,3 @@ class Categorias(models.Model):
     def __str__(self):
         return self.nombre
     
-class CategoriasFilter(django_filters.FilterSet):
-    nombre = django_filters.CharFilter(field_name='nombre', lookup_expr='icontains')
-    
-    class Meta:
-        model = Categorias
-        fields = ['nombre']
--- backend/ecommerce/categoria/views.py
+++ backend/ecommerce/categoria/views.py
@@ -1,19 +1,11 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .serializers import CategoriasSerializer
-from .models import Categorias, CategoriasFilter
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination
+from .models import Categorias
 
-class CategoriasPagination(PageNumberPagination):
-    page_size = 10  
-    page_size_query_param = 'page_size'
-    max_page_size = 100
 
 class CategoriasList(generics.ListCreateAPIView):
     queryset = Categorias.objects.all() 
-    serializer_class = CategoriasSerializer  
-    pagination_class = CategoriasPagination  
+    serializer_class = CategoriasSerializer    
     filter_backends = (DjangoFilterBackend,)  
     filterset_class = CategoriasFilter  
 
