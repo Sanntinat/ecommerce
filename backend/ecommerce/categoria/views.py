e4d1cdb28c719fee9d242a17c6dd09a9271b2664
Sanntinat <messinatalichio@gmail.com>
2024-09-23 20:21:04 -0300

    fix

--- backend/ecommerce/categoria/views.py
+++ backend/ecommerce/categoria/views.py
@@ -6,8 +6,6 @@ from .models import Categorias
 class CategoriasList(generics.ListCreateAPIView):
     queryset = Categorias.objects.all() 
     serializer_class = CategoriasSerializer    
-    filter_backends = (DjangoFilterBackend,)  
-    filterset_class = CategoriasFilter  
 
 class CategoriasDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categorias.objects.all()  
