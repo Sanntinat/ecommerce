d09cb891aaebec51d4c10c09aa59e67c1fdaf866
mariano martinez <martinez25mariano25@gmail.com>
2024-12-09 19:50:50 -0300

    footer ya no sale en admin

--- frontend/E-commerce/src/Componentes/Principal/footer.jsx
+++ frontend/E-commerce/src/Componentes/Principal/footer.jsx
@@ -4,9 +4,12 @@ import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid' 
 import { Facebook, Twitter, Instagram } from '@mui/icons-material';
 import { GoogleMap, LoadScript, Marker, LoadScriptNext } from '@react-google-maps/api';
+import { useLocation } from 'react-router-dom';
+
 
 const Footer = () => {
-
+	
+	const url = useLocation();
 	const containerStyle = {
   		width: '90%',
 		height: '200px',
@@ -19,32 +22,32 @@ const Footer = () => {
  		lng: -58.381559,
 	};
 
-  return (
-    <Box sx={{ width: '100%', backgroundColor: '#1976d2', color: 'white', mb: 0}}>
-      <Grid container justifyContent="space-between">
-        <Grid item xs={5}>
-     		<LoadScriptNext googleMapsApiKey="AIzaSyDSY2z4DrAMaUtFBEkM9_Mn_DtMAeW9X2A">
-	  		<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-         	 <Marker position={center} />
-        	</GoogleMap>
-		</LoadScriptNext>
-		</Grid>
-		<Grid item xs={3} sx={{textAlign: 'left', pt: 3}}>
-		 	<Typography variant="h5" sx={{ textAlign: 'left', pl:0 , ml:0 }}>Ubicación</Typography>
-			 <Typography variant="body2">calle falsa 123, Quilmes</Typography>
-		 </Grid>
-
-		<Grid item xs={12} sm={4}>
-		  <Typography variant="h4" sx={{mb: 1, mt:6 }}>Contactanos</Typography>
-          <Typography variant="body2">Tel: (123) 456-7890</Typography>
-		  <Typography variant="body2">Email: powerFit@gmail.com.ar</Typography>
-        <Typography variant="body2">&copy; 2024 PowerFit. Todos los derechos reservados.</Typography>
-
-        </Grid>
-      </Grid>
+	return (
+		url.pathname !== '/admin' && (
+	    	<Box sx={{ width: '100%', backgroundColor: '#1976d2', color: 'white', mb: 0}}>
+      		<Grid container justifyContent="space-between">
+        	<Grid item xs={5}>
+     			<LoadScriptNext googleMapsApiKey="AIzaSyDSY2z4DrAMaUtFBEkM9_Mn_DtMAeW9X2A">
+	  				<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+         	 			<Marker position={center} />
+        			</GoogleMap>
+				</LoadScriptNext>
+			</Grid>
+			<Grid item xs={3} sx={{textAlign: 'left', pt: 3}}>
+		 		<Typography variant="h5" sx={{ textAlign: 'left', pl:0 , ml:0 }}>Ubicación</Typography>
+			 	<Typography variant="body2">calle falsa 123, Quilmes</Typography>
+		 	</Grid>
+			<Grid item xs={12} sm={4}>
+		 		<Typography variant="h4" sx={{mb: 1, mt:6 }}>Contactanos</Typography>
+          		<Typography variant="body2">Tel: (123) 456-7890</Typography>
+		  		<Typography variant="body2">Email: powerFit@gmail.com.ar</Typography>
+        		<Typography variant="body2">&copy; 2024 PowerFit. Todos los derechos reservados.</Typography>
+        	</Grid>
+      	</Grid>
       <Box sx={{ textAlign: 'center', marginTop: 2 }}>
       </Box>
-    </Box>
+  	</Box>
+	)
   );
 };
 
