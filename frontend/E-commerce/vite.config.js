1d97b48c652969f7549c3e178885a1669450399f
Sanntinat <messinatalichio@gmail.com>
2025-02-27 21:57:18 -0300

    prueba

--- frontend/E-commerce/vite.config.js
+++ frontend/E-commerce/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,   // Asegura que Docker detecte cambios en los archivos
+    },
+  },
+  optimizeDeps: {
+    exclude: [
+      '@mui/material',
+      '@mui/icons-material',
+      'react-router-dom',
+      '@react-google-maps/api'
+    ],
+  },
 })
