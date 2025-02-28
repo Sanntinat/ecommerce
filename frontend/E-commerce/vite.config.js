b6250985efff10d0e20d6ca7adbe49025932f241
Sanntinat <messinatalichio@gmail.com>
2025-02-28 16:04:37 -0300

    fix error

--- frontend/E-commerce/vite.config.js
+++ frontend/E-commerce/vite.config.js
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Asegura que Vite escuche en todas las interfaces
+    host: '0.0.0.0', // Escucha en todas las interfaces
     port: 5173, // Puerto para evitar conflictos
+    hmr: {
+      // Configura el HMR para que use el mismo dominio que el cliente
+      host: 'ecommerce-production-0212.up.railway.app',
+      port: 443, // Usa el puerto 443 para HTTPS
+      protocol: 'wss', // Usa WebSocket seguro (WSS) en producción
+    },
   },
   optimizeDeps: {
     exclude: [
-    'react',
-    'react-dom',
-    '@mui/material',
-    '@mui/icons-material',
-    'react-router-dom',
-    'prop-types',
-    'react-swipeable-views-utils',
-  ]
+      'react',
+      'react-dom',
+      '@mui/material',
+      '@mui/icons-material',
+      'react-router-dom',
+      'prop-types',
+      'react-swipeable-views-utils',
+    ],
   },
-})
-
+});
